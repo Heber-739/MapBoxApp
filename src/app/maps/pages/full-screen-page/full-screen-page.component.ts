2830a96358811430f04520ce7ab17a149f1197ae
Heber Duarte <heberduarteryr@gmail.com>
2023-07-19 09:43:26 -0300

    Show fullscreen map

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import * as mapboxgl from 'mapbox-gl';
+
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiaGViZXI3MyIsImEiOiJjbGppd2x1MXEwM256M2tvN3d4cG84M3dpIn0.r50Tp_Sfq5WGqB7oc4JTqw';
+
 
 @Component({
   selector: 'app-full-screen-page',
   templateUrl: './full-screen-page.component.html',
-  styles: [
-  ]
+  styles: ['#map{width:100vw;height:100vh;background-color:red;}']
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit{
+
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+      });
+  }
+
 
 }
