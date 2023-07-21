059ae2315a84775ba8248936f9b2089b11f4a606
Heber Duarte <heberduarteryr@gmail.com>
2023-07-21 12:32:47 -0300

    Standalone components

--- src/app/alone/components/side-menu/side-menu.component.ts
+++ src/app/alone/components/side-menu/side-menu.component.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 interface MenuItem{
   route: string;
   name:string;
 }
 @Component({
-  selector: 'maps-side-menu',
-  templateUrl: './side-menu.component.html',
-  styles: ['li{cursor:pointer;transition:0.2s all;}']
+  selector: 'side-menu',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+    templateUrl: './side-menu.component.html',
+  styles: ['li{cursor:pointer;transition:0.2s all;}ul{position:fixed;top:30px;left:20px;z-index:999;}']
 })
 export class SideMenuComponent {
 
@@ -16,6 +20,7 @@ export class SideMenuComponent {
     {route:'/maps/zoom-range', name: 'Zoom-range'},
     {route:'/maps/markers', name: 'Markers'},
     {route:'/maps/properties', name: 'Houses'},
+    {route:'/alone', name: 'Alone Page'},
   ]
 
 }
--- src/app/maps/layout/maps-layout/maps-layout.component.ts
+++ src/app/maps/layout/maps-layout/maps-layout.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-maps-layout',
   templateUrl: './maps-layout.component.html',
-  styles: ['maps-side-menu{position:fixed;top:30px;left:20px;z-index:999;}']
+  styles: []
 })
 export class MapsLayoutComponent {
 
